caa9b19d7e2a267d44894146255d261db48a3c20
Isibor Jerry <47747113+saturnarians@users.noreply.github.com>
2024-08-10 04:59:33 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,30 +11,7 @@ import Footer from "../components/Footer";
 const HomePage = () => {
   return (
     <>
-    <div className="h-full font-[sans-serif] bg-center bg-cover w-[auto]">
-      <div className=" ">
-      <Header />
-      </div>
-      <div className=" ">
-      <Navbar className=' place-self-center'/>
-      </div>
-      <div className="">
-      <Hero className='' />
-      </div>
-      
-      <div className="">
-        <QuoteForm />
-      </div>
-
-      <div className="place-self-center">
-      <Button />
-      </div>
-
-      <div className="place-self-center">
-      <Footer />
-      </div>
-      
-     </div>
+   
     </>
   );
 };
